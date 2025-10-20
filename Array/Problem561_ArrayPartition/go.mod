efd1cd934477e3ec6b2d321243756fc9973ca099
vimalkuriensam <vimalkuriensam@gmail.com>
2025-10-21 00:44:05 +0530

    added solution to problem 561

---
+++ Array/Problem561_ArrayPartition/go.mod
@@ -0,0 +1,3 @@
+module github.com/vimalkuriensam/algorithms/arrays/problem561_arraypartition
+
+go 1.23.1
---
+++ Array/Problem561_ArrayPartition/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func arrayPairSum(nums []int) int {
+	sort.Ints(nums)
+	sum := 0
+	for i := 0; i < len(nums); i = i + 2 {
+		sum += nums[i]
+	}
+	return sum
+}
+
+func main() {
+	fmt.Println(arrayPairSum([]int{1, 4, 3, 2}))
+}
