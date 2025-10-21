6d89d6deb5e7266bbd66c0a67b70d89e681cee3c
vimalkuriensam <vimalkuriensam@gmail.com>
2025-10-21 21:32:39 +0530

    added solution to problem 35

---
+++ Array/Problem35_SearchInsertPosition/go.mod
@@ -0,0 +1,3 @@
+module github.com/vimalkuriensam/algorithms/array/problem35_searchinsertposition
+
+go 1.23.1
---
+++ Array/Problem35_SearchInsertPosition/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func searchInsert(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left < right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	if nums[left] < target {
+		return left + 1
+	}
+	return left
+}
+
+func main() {
+	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 5))
+	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 2))
+	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 7))
+}
